939eaf94737af1374cdde8379cd2e85cb0b1b035
olgacsmendes <olgacsmendes@gmail.com>
2022-12-20 13:19:14 -0300

    first commit

---
+++ src/Backend/data.js
@@ -0,0 +1,55 @@
+import imgOne from './images/01.jpg';
+import imgTwo from './images/02.jpg';
+import imgThree from './images/03.jpg';
+import imgFour from './images/04.jpg';
+import imgFive from './images/05.jpg';
+import imgSix from './images/06.jpg';
+
+const data = { 
+    products: [
+        {
+            id: 1,
+            img: imgOne,
+            title: 'Square One (2016)',
+            desc: 'BLACKPINK',
+            price: 50,
+        },
+        {
+            id: 2,
+            img: imgTwo,
+            title: 'Square Two (2016)',
+            desc: 'BLACKPINK',
+            price: 50,
+        },
+        {
+            id: 3,
+            img: imgThree,
+            title: 'Square Up (2018)',
+            desc: 'BLACKPINK',
+            price: 80,
+        },
+        {
+            id: 4,
+            img: imgFour,
+            title: 'Kill this Love (2019)',
+            desc: 'BLACKPINK',
+            price: 80,
+        },
+        {
+            id: 5,
+            img: imgFive,
+            title: 'The Album (2020)',
+            desc: 'BLACKPINK',
+            price: 100,
+        },
+        {
+            id: 6,
+            img: imgSix,
+            title: 'Born Pink (2022)',
+            desc: 'BLACKPINK',
+            price: 100,
+        },
+    ]
+}
+
+export default data;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,23 @@
+import Cards from '../../src/Components/Cards/Cards';
+import data from '../Backend/data';
+import './Home.css';
+
+
+const Home = () => {
+  return (
+    <div className="container-fluid">
+        <h3 className="text-center mt-5 text-uppercase">Lojinha</h3>
+        <div className="container py-4">
+            <div className="row">
+              {data.products.map((item, index) => {
+                return (
+                  <Cards img={item.img} title={item.title} desc={item.desc} price={item.price} item={item} key={index} />
+                )
+              })}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
